bce773ba90dc4760da85de3acfa49bf0ce674665
Azarul Mohammed <theazarul100@gmail.com>
2023-08-23 21:47:28 -0400

    hero updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,11 +8,15 @@ const Hero = () => {
     <section className='relative w-full h-screen mx-auto'>
       <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7x1 mx-auto flex flex-row items-start gap-5`}>
         <div className='flex flex-col justify-center items-center mt-5'>
-          <div className='w-5 h-5 rounded-full bg-[#915eff]'>
-
-          </div>
+          <div className='w-5 h-5 rounded-full bg-[#915eff]'/>
+          <div className='w-1 sm:h-80 h-40 violet-gradient'/>
+        </div>
+        <div>
+          <h1 className={`${styles.heroHeadText}text-white`}>Hi, I'm <span className='text-[#ab0000] '>Azarul</span></h1>
+          <p className={`${styles.heroSubText}mt-2 text-white-100`}>Enthusiastic CS Graduate Eager to Contribute</p>
         </div>
       </div>
+      <ComputersCanvas/>
     </section>
   )
 }
